9987dbdf53b2b5ae5fecab2e054950521eedd0b7
Zac Driscoll <zgdriscoll@gmail.com>
2022-02-24 14:03:17 -0600

    built a shiny dashboards ui

--- R/app_server.R
+++ R/app_server.R
@@ -1,10 +1,11 @@
-#' The application server-side
-#' 
-#' @param input,output,session Internal parameters for {shiny}. 
-#'     DO NOT REMOVE.
 #' @import shiny
-#' @noRd
-app_server <- function( input, output, session ) {
-  # Your application server logic 
+app_server <- function(input, output,session) {
+  # List the first level callModules here
+  set.seed(122)
+  histdata <- rnorm(500)
   
+  output$plot1 <- shiny::renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -1,42 +1,66 @@
-#' The application User-Interface
-#' 
-#' @param request Internal parameter for `{shiny}`. 
-#'     DO NOT REMOVE.
+#' ui
+#'
+#' @param request needed for bookmarking
+#'
 #' @import shiny
-#' @noRd
+#' @import shinydashboard
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
-    golem_add_external_resources(),
-    # Your application UI logic 
-    fluidPage(
-      h1("troutViewer")
+    # golem_add_external_resources(),
+    # List the first level UI elements here
+    shinydashboard::dashboardPage(
+      shinydashboard::dashboardHeader(title = "Basic dashboard"),
+      shinydashboard::dashboardSidebar(
+        shinydashboard::sidebarMenu(
+          shinydashboard::menuItem(
+            "Dashboard",
+            tabName = "dashboard",
+            icon = icon("dashboard")
+          ),
+          shinydashboard::menuItem("Widgets", 
+                                   tabName = "widgets",
+                                   icon = icon("th"))
+        )
+      ),
+      shinydashboard::dashboardBody(
+        shinydashboard::tabItems(
+          # First tab content
+          shinydashboard::tabItem(
+            tabName = "dashboard",
+            shiny::fluidRow(
+              shinydashboard::box(plotOutput("plot1", height = 250)),
+              shinydashboard::box(
+                title = "Controls",
+                sliderInput("slider", "Number of observations:", 1, 100, 50)
+              )
+            )
+          ),
+          # Second tab content
+          shinydashboard::tabItem(tabName = "widgets",
+                                  h2("Widgets tab content"))
+        )
+      )
     )
   )
 }
 
-#' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
 #' @import shiny
-#' @importFrom golem add_resource_path activate_js favicon bundle_resources
-#' @noRd
 golem_add_external_resources <- function(){
   
-  add_resource_path(
-    'www', app_sys('app/www')
+  addResourcePath(
+    'www', system.file('app/www', package = "golemdashboard")
   )
- 
+  
   tags$head(
-    favicon(),
-    bundle_resources(
-      path = app_sys('app/www'),
-      app_title = 'troutViewer'
-    )
-    # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
+    golem::activate_js(),
+    golem::favicon(),
+    tags$title("golemdashboard")
+    # Add here all the external resources
+    # If you have a custom.css in the inst/app/www
+    # Or for example, you can add shinyalert::useShinyalert() here
+    #tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
+    
   )
 }
 
