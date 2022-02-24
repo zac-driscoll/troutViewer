6d2ab701c0eee5d921e4dd4c7c35094b7ee4971e
Zac Driscoll <zgdriscoll@gmail.com>
2022-02-24 14:13:10 -0600

    added a plotly ipsum function

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -25,7 +25,7 @@ usethis::use_package("shinipsum")
 usethis::use_package("trout.modlR")
 ## Add modules ----
 ## Create a module infrastructure in R/
-golem::add_module( name = "name_of_module1" ) # Name of the module
+golem::add_module( name = "ipsum_plot" ) # Name of the module
 golem::add_module( name = "name_of_module2" ) # Name of the module
 
 ## Add helper functions ----
