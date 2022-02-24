1bf19e9ebe7d3e17470ea25f1e7af5e1d67445cd
Zac Driscoll <zgdriscoll@gmail.com>
2022-02-24 14:44:25 -0600

    created a load_data module

---
+++ R/mod_load_data.R
@@ -0,0 +1,38 @@
+#' load_data UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_load_data_ui <- function(id){
+  shiny::tagList(
+    shiny::fileInput(
+      shiny::NS(id, "modelData"),
+      "Select Model Output"),
+    shiny::actionButton(
+      shiny::NS(id,"data_button"),
+      "Get Data")
+  )
+}
+    
+#' load_data Server Functions
+#'
+#' @noRd 
+mod_load_data_server <- function(id) {
+  shiny::moduleServer(id, function(input, output, session) {
+    shiny::eventReactive(input$data_button, {
+      shiny::req(input$modelData)
+      dget(input$modelData$datapath)
+    })
+  })
+}
+
+    
+## To be copied in the UI
+# mod_load_data_ui("load_data_ui_1")
+    
+## To be copied in the server
+# mod_load_data_server("load_data_ui_1")
