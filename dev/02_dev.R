1c4ab035ff8d85602e516855cc71a52521034acd
Zac Driscoll <zgdriscoll@gmail.com>
2022-02-25 06:46:41 -0600

    removed cached

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -26,7 +26,7 @@ usethis::use_package("trout.modlR")
 ## Add modules ----
 ## Create a module infrastructure in R/
 golem::add_module( name = "ipsum_plot" ) # Name of the module
-golem::add_module( name = "name_of_module2" ) # Name of the module
+golem::add_module( name = "load_data" ) # Name of the module
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
